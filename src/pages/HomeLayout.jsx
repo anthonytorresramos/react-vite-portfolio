931a374b79fd460e4e67922607510f85e4c10de4
anthony ramos <anthonytorresramos3@gmail.com>
2023-06-23 23:09:27 +0800

    hide project2 but will be use in the future

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,22 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import { useState } from "react";
+
 import MyProjects from "../components/MyProjects";
-import MyProject2 from "../components/MyProject2";
+// import MyProject2 from "../components/MyProject2";
 
 const HomeLayout = () => {
-  //   const [videoLoaded, setVideoLoaded] = useState(false);
-  //   const handleVideoLoad = () => {
-  //     setVideoLoaded(true);
-  //   };
-
   return (
     <section>
       <Header />
       <Hero />
       <MyProjects />
-      <MyProject2 />
+      {/* <MyProject2 /> */}
       <Outlet />
     </section>
   );
