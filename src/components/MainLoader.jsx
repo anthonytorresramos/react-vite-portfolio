e4915a11c063da5d3123bf3b0b97816949874777
anthony ramos <anthonytorresramos3@gmail.com>
2023-06-23 14:15:56 +0800

    add thumbnail on React Player and MainLoader.jsx to be use later

---
+++ src/components/MainLoader.jsx
@@ -0,0 +1,8 @@
+const MainLoader = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+export default MainLoader;
